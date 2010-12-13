c9ed412ca2817e0d508a67ed3e61bfeb2ca6ed70
rightgo09 <skyarrow09@gmail.com>
2010-12-13 14:32:45 +0900

    base.

---
+++ lib/URI/Tag.pm
@@ -0,0 +1,15 @@
+package URI::Tag;
+
+# $Id$
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+use URI;
+
+
+1;
+__END__
+
---
+++ t/00_compile.t
@@ -0,0 +1,10 @@
+use strict;
+use utf8;
+
+use Test::More tests => 1;
+
+BEGIN { use_ok "URI::Tag" }
+
+
+__END__
+
